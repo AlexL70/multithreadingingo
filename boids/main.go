d4ade48c965d10b2be608f87272a96b703049b44
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-04-27 20:59:24 +0300

    9. Memory Sharing example Part 1

--- boids/main.go
+++ boids/main.go
@@ -10,11 +10,14 @@ import (
 const (
 	screenWidth, screenHeight = 640, 360
 	boidCount                 = 500
+	viewRadius                = 15
+	adjRate                   = 0.015
 )
 
 var (
-	green = color.RGBA{R: 10, G: 255, B: 10, A: 255}
-	boids [boidCount]*Boid
+	green   = color.RGBA{R: 10, G: 255, B: 10, A: 255}
+	boids   [boidCount]*Boid
+	boidMap [screenWidth + 1][screenHeight + 1]int
 )
 
 type Game struct{}
@@ -37,6 +40,11 @@ func (g Game) Layout(_, _ int) (w, h int) {
 }
 
 func main() {
+	for i, row := range boidMap {
+		for j, _ := range row {
+			boidMap[i][j] = -1
+		}
+	}
 	for i := 0; i < boidCount; i++ {
 		createBoid(i)
 	}
