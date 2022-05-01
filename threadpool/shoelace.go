346309afbfd549bef8dc385fa109f0a47284fbe5
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-05-01 17:44:50 +0300

    22. Thread Pool Example Part 2

--- threadpool/shoelace.go
+++ threadpool/shoelace.go
@@ -7,6 +7,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -15,32 +16,46 @@ type Point2D struct {
 	y int
 }
 
+const numberOfThreads int = 8
+
 var (
-	r = regexp.MustCompile(`\((\d*),(\d*)\)`)
+	r         = regexp.MustCompile(`\((\d*),(\d*)\)`)
+	waitGroup = sync.WaitGroup{}
 )
 
-func findArea(pointsString string) {
-	var points []Point2D
-	for _, p := range r.FindAllStringSubmatch(pointsString, -1) {
-		x, _ := strconv.Atoi(p[1])
-		y, _ := strconv.Atoi(p[2])
-		points = append(points, Point2D{x: x, y: y})
+func findArea(inputChannel chan string) {
+	for pointsString := range inputChannel {
+		var points []Point2D
+		for _, p := range r.FindAllStringSubmatch(pointsString, -1) {
+			x, _ := strconv.Atoi(p[1])
+			y, _ := strconv.Atoi(p[2])
+			points = append(points, Point2D{x: x, y: y})
+		}
+		area := 0.0
+		for i := 0; i < len(points); i++ {
+			a, b := points[i], points[(i+1)%len(points)]
+			area += float64(a.x*b.y) - float64(a.y*b.x)
+		}
+		fmt.Println(math.Abs(area) / 2.0)
 	}
-	area := 0.0
-	for i := 0; i < len(points); i++ {
-		a, b := points[i], points[(i+1)%len(points)]
-		area += float64(a.x*b.y) - float64(a.y*b.x)
-	}
-	fmt.Println(math.Abs(area) / 2.0)
+	waitGroup.Done()
 }
 
 func main() {
 	dat, _ := ioutil.ReadFile("polygons.txt")
 	text := string(dat)
+
+	inputChannel := make(chan string, 1000)
 	start := time.Now()
-	for _, line := range strings.Split(text, "\n") {
-		findArea(line)
+	for i := 0; i < numberOfThreads; i++ {
+		go findArea(inputChannel)
 	}
+	waitGroup.Add(numberOfThreads)
+	for _, line := range strings.Split(text, "\n") {
+		inputChannel <- line
+	}
+	close(inputChannel)
+	waitGroup.Wait()
 	elapsed := time.Since(start)
 	fmt.Printf("Finding all areas took %s.\n", elapsed)
 }
