99b13c3c6573156a5812274eb434713e5fbc7e1a
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-05-03 22:42:16 +0300

    36. Barriers Explained

---
+++ barriers/barrier_example.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"time"
+)
+
+func waitOnBarrier(name string, timeToSleep int, barrier *Barrier) {
+	for {
+		println(name, "running")
+		time.Sleep(time.Duration(timeToSleep) * time.Second)
+		println(name, "is waiting on barrier")
+		barrier.Wait()
+	}
+}
+
+func main() {
+	barrier := NewBarrier(2)
+	go waitOnBarrier("red", 4, barrier)
+	go waitOnBarrier("blue", 5, barrier)
+	time.Sleep(time.Duration(30) * time.Second)
+}
---
+++ barriers/go.mod
@@ -0,0 +1,3 @@
+module github.com/AlexL70/multithreadingingo/barriers
+
+go 1.18
