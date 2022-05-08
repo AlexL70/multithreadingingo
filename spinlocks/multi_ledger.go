511e83d4c2cec4b513744856a422cc7f68d0ab52
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-05-08 17:44:25 +0300

    47. Implementing the Ledger Part 2

--- spinlocks/multi_ledger.go
+++ spinlocks/multi_ledger.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"sort"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 const (
@@ -28,11 +30,36 @@ func performMovements(ledger *[totalAccounts]int32, locks *[totalAccounts]sync.L
 		locks[toLock[1]].Lock()
 		atomic.AddInt32(&ledger[accountA], -amountToMove)
 		atomic.AddInt32(&ledger[accountB], amountToMove)
+		atomic.AddInt64(totalTrans, 1)
 		locks[toLock[1]].Unlock()
 		locks[toLock[0]].Unlock()
 	}
 }
 
 func main() {
-
+	fmt.Println("Total accounts:", totalAccounts, ", total threads:", threads, "using SpinLocks")
+	var ledger [totalAccounts]int32
+	var locks [totalAccounts]sync.Locker
+	var totalTrans int64
+	for i := 0; i < totalAccounts; i++ {
+		ledger[i] = initialMoney
+		locks[i] = NewSpinLock()
+	}
+	for i := 0; i < threads; i++ {
+		go performMovements(&ledger, &locks, &totalTrans)
+	}
+	for {
+		time.Sleep(2 * time.Second)
+		var sum int32
+		for i := 0; i < totalAccounts; i++ {
+			locks[i].Lock()
+		}
+		for i := 0; i < totalAccounts; i++ {
+			sum += ledger[i]
+		}
+		for i := 0; i < totalAccounts; i++ {
+			locks[i].Unlock()
+		}
+		fmt.Printf("Transactions performed: %d. Balance: %d\n", totalTrans, sum)
+	}
 }
