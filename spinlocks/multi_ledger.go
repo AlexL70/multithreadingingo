9fe6f39401dd7572f92cff89336d2eb9cad6c645
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-05-08 17:12:16 +0300

    46. Implementing the Ledger Part 1

---
+++ spinlocks/multi_ledger.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"math/rand"
+	"sort"
+	"sync"
+	"sync/atomic"
+)
+
+const (
+	totalAccounts  = 50000
+	maxAmountMoved = 10
+	initialMoney   = 100
+	threads        = 4
+)
+
+func performMovements(ledger *[totalAccounts]int32, locks *[totalAccounts]sync.Locker, totalTrans *int64) {
+	for {
+		accountA := rand.Intn(totalAccounts)
+		accountB := rand.Intn(totalAccounts)
+		for accountA == accountB {
+			accountB = rand.Intn(totalAccounts)
+		}
+		amountToMove := rand.Int31n(maxAmountMoved)
+		toLock := []int{accountA, accountB}
+		sort.Ints(toLock)
+		locks[toLock[0]].Lock()
+		locks[toLock[1]].Lock()
+		atomic.AddInt32(&ledger[accountA], -amountToMove)
+		atomic.AddInt32(&ledger[accountB], amountToMove)
+		locks[toLock[1]].Unlock()
+		locks[toLock[0]].Unlock()
+	}
+}
+
+func main() {
+
+}
