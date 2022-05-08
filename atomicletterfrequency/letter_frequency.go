7fcc4a06de3a9c71798a370898943259cc078ec2
Alexander Levinson <alexander.levinson.70@gmail.com>
2022-05-08 15:24:11 +0300

    42. Using Atomic Variables in our Example

--- atomicletterfrequency/letter_frequency.go
+++ atomicletterfrequency/letter_frequency.go
@@ -5,42 +5,46 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
 const allLetters = "abcdefghijklmnopqrstuvwxyz"
 
-func countLetters(url string, frequency *[26]int) error {
+func countLetters(url string, frequency *[26]int32, wg *sync.WaitGroup) {
+	defer wg.Done()
 	resp, err := http.Get(url)
 	if err != nil {
 		err = fmt.Errorf("Error opening page %s: %w", url, err)
-		return err
+		fmt.Println(err)
+		return
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		err = fmt.Errorf("Error reading body. Url: %s. Error: %w", url, err)
-		return err
+		fmt.Println(err)
+		return
 	}
 	for _, b := range body {
 		c := strings.ToLower(string(b))
 		index := strings.Index(allLetters, c)
 		if index >= 0 {
-			frequency[index]++
+			atomic.AddInt32(&frequency[index], 1)
 		}
 	}
-	return nil
 }
 
 func main() {
-	var frequency [26]int
+	var frequency [26]int32
+	wg := sync.WaitGroup{}
 	start := time.Now()
 	for i := 1000; i < 1200; i++ {
-		err := countLetters(fmt.Sprintf("https://www.rfc-editor.org/rfc/rfc%d.txt", i), &frequency)
-		if err != nil {
-			fmt.Println(err)
-		}
+		wg.Add(1)
+		go countLetters(fmt.Sprintf("https://www.rfc-editor.org/rfc/rfc%d.txt", i), &frequency, &wg)
 	}
+	wg.Wait()
 	elapsed := time.Since(start)
 	fmt.Printf("Processing took %s\n", elapsed)
 	for i, f := range frequency {
